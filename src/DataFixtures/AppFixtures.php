1487272a5e2d5e4e35c446c1985e94146656b997
Krzysztof Sulej <kaps@o2.pl>
2024-01-05 17:03:45 +0100

    Dodano fixtures oraz genrowanie 10k fałszywych kontaktów

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,25 +5,13 @@ namespace App\DataFixtures;
 use App\Entity\Contact;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Factory\ContactFactory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-
-        $contact = new Contact();
-        $contact->setFirstName('Adam')
-            ->setLastName('Nowak')
-            ->setCompany('Firma Test');
-        if (rand(1, 10) > 2) {
-            $contact->setCreatedAt(new \DateTimeImmutable(sprintf('-%d days', rand(1, 100))));
-        }
-
-
-
-        // $product = new Product();
-        $manager->persist($contact);
-
+        ContactFactory::new()->createMany(10000);
         $manager->flush();
     }
 }
